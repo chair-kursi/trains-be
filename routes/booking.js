33c2915cc35dfb9ab5d39432a037a82d64b033aa
Vaibhav <vkpstudyy247@gmail.com>
2022-04-06 10:58:21 +0530

    passengers specific fix

--- routes/booking.js
+++ routes/booking.js
@@ -6,17 +6,7 @@ const Booking = require('../models/Booking');
 router.post("/booking", async (req, res)=>{
     try {
         const booking = new Booking(req.body);
-        const passengerName = req.body.passengerName;
-        const existingPassenger = Booking.findOne({passengerName: passengerName});
-        if(existingPassenger){
-            try{
-                Booking.updateOne({passengerName: passengerName}, {booking: [...existingPassenger.booking, req.body.booking]});
-                res.send("Train Booked");
-            }catch(err){
-                res.send(`Booking failed due to ${err}`);
-            }
-            return;
-        }
+        const passengerName = req.body.passengerName; 
         const booked = await booking.save();
         res.json({booked: booked});
     } catch (err) {
--- routes/passengers.js
+++ routes/passengers.js
@@ -6,7 +6,12 @@ const Passenger = require('../models/Passenger');
 router.get("/passenger", async (req, res)=>{
     const passenger = await Passenger.find({});
     res.json({passenger: passenger});
-})
+});
+
+router.get("/passenger/:name", async (req, res)=>{
+    const passenger = await Passenger.find({passengerName: req.params.name});
+    res.json({passenger: passenger});
+});
 
 module.exports = router;
  
